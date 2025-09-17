a4374d625ddf5bc0cf339954ed5461a89d630f51
gokhankkaraduman <gokhan.kkaraduman3@gmail.com>
2025-09-17 14:37:48 +0300

    initial files

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './css/reset.css'
+import './css/global.css'
+import App from './Components/App/App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
